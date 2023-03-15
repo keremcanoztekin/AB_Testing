cceebdc9c162ca37310cb7a069385c1d6d21a44c
Kerem Can Öztekin <103461795+keremcanoztekin@users.noreply.github.com>
2023-03-15 18:20:17 +0300

    Update AB_TESTING.py

--- AB_TESTING.py
+++ AB_TESTING.py
@@ -2,73 +2,11 @@
 # Comparison of Bidding Methods Conversions with AB Test
 #####################################################
 # Importing required libraries
-import pandas as pd
-
-#####################################################
-# İş Problemi
-#####################################################
-
-#
-# teklif verme türüne alternatif
-# olarak yeni bir teklif türü olan "average bidding"’i tanıttı. Müşterilerimizden biri olan bombabomba.com,
-# bu yeni özelliği test etmeye karar verdi veaveragebidding'in maximumbidding'den daha fazla dönüşüm
-# getirip getirmediğini anlamak için bir A/B testi yapmak istiyor.A/B testi 1 aydır devam ediyor ve
-# bombabomba.com şimdi sizden bu A/B testinin sonuçlarını analiz etmenizi bekliyor.Bombabomba.com için
-# nihai başarı ölçütü Purchase'dır. Bu nedenle, istatistiksel testler için Purchasemetriğine odaklanılmalıdır.
-
-
-
-
-#####################################################
-# Veri Seti Hikayesi
-#####################################################
-
-# Bir firmanın web site bilgilerini içeren bu veri setinde kullanıcıların gördükleri ve tıkladıkları
-# reklam sayıları gibi bilgilerin yanı sıra buradan gelen kazanç bilgileri yer almaktadır.Kontrol ve Test
-# grubu olmak üzere iki ayrı veri seti vardır. Bu veri setleriab_testing.xlsxexcel’ininayrı sayfalarında yer
-# almaktadır. Kontrol grubuna Maximum Bidding, test grubuna AverageBiddinguygulanmıştır.
-
-# impression: Reklam görüntüleme sayısı
-# Click: Görüntülenen reklama tıklama sayısı
-# Purchase: Tıklanan reklamlar sonrası satın alınan ürün sayısı
-# Earning: Satın alınan ürünler sonrası elde edilen kazanç
-
-
-
-#####################################################
-# Proje Görevleri
-#####################################################
-
-######################################################
-# AB Testing (Bağımsız İki Örneklem T Testi)
-######################################################
-
-# 1. Hipotezleri Kur
-# 2. Varsayım Kontrolü
-#   - 1. Normallik Varsayımı (shapiro)
-#   - 2. Varyans Homojenliği (levene)
-# 3. Hipotezin Uygulanması
-#   - 1. Varsayımlar sağlanıyorsa bağımsız iki örneklem t testi
-#   - 2. Varsayımlar sağlanmıyorsa mannwhitneyu testi
-# 4. p-value değerine göre sonuçları yorumla
-# Not:
-# - Normallik sağlanmıyorsa direkt 2 numara. Varyans homojenliği sağlanmıyorsa 1 numaraya arguman girilir.
-# - Normallik incelemesi öncesi aykırı değer incelemesi ve düzeltmesi yapmak faydalı olabilir.
-
-
-
-
-#####################################################
-# Görev 1:  Veriyi Hazırlama ve Analiz Etme
-#####################################################
-
-# Adım 1:  ab_testing_data.xlsx adlı kontrol ve test grubu verilerinden oluşan veri setini okutunuz. Kontrol ve test grubu verilerini ayrı değişkenlere atayınız.
 import itertools
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-# !pip install statsmodels
 import statsmodels.stats.api as sms
 from scipy.stats import ttest_1samp, shapiro, levene, ttest_ind, mannwhitneyu, \
     pearsonr, spearmanr, kendalltau, f_oneway, kruskal
@@ -76,12 +14,30 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', 10)
 pd.set_option('display.float_format', lambda x: '%.5f' % x)
 
+######################################################
+# AB Testing (Bağımsız İki Örneklem T Testi)
+######################################################
+
+#1. Establish Hypotheses
+#2. Assumption Check
+# - 1. Normality Assumption (shapiro)
+# - 2. Variance Homogeneity (levene)
+# 3. Implementation of the Hypothesis
+# - 1. Independent two-sample t-test if assumptions are met
+# - 2. Mannwhitneyu test if assumptions are not provided
+# 4. Interpret results based on p-value
+
+
+
+#####################################################
+# Data Preparation 
+#####################################################
+
 # Assign control and test group data to separate variables.
 control_df = pd.read_excel("ABTesti/ab_testing.xlsx", sheet_name="Control Group")
 test_df = pd.read_excel("ABTesti/ab_testing.xlsx", sheet_name="Test Group")
 
 # Combining control and test group data
-
 control_df.columns = [col + "_control" for col in control_df.columns]
 test_df.columns = [col + "_test" for col in test_df.columns]
 df = pd.concat([control_df,test_df], axis=1)
@@ -91,14 +47,11 @@ df = pd.concat([control_df,test_df], axis=1)
 # Defining A/B Test Hypothesis
 #####################################################
 
-# Step 1: Hipotezi tanımlayınız.
+# Step 1: Define the hypothesis.
 # H0: There is no statistically significant difference between the purchase averages of Maximum Bidding and Average Bidding.
 # H1:There is a statistically significant difference between the purchase averages of Maximum Bidding and Average Bidding.
 
 
-
-# Adım 2: Kontrol ve test grubu için purchase(kazanç) ortalamalarını analiz ediniz
-df[["Purchase_control", "Purchase_test"]].mean()
 
 #####################################################
 # GÖREV 3: Implementation of Hypothesis Testing
@@ -106,10 +59,8 @@ df[["Purchase_control", "Purchase_test"]].mean()
 
 
 ######################################################
-# AB Testing (Bağımsız İki Örneklem T Testi)
-######################################################
+# Assumption Control
 
-# Adım 1: Hipotez testi yapılmadan önce varsayım kontrollerini yapınız.Bunlar Normallik Varsayımı ve Varyans Homojenliğidir.
 
 # Testing separately whether the control and test group comply with the normality assumption via the Purchase variable.
 # Normality Assumption
@@ -125,8 +76,10 @@ test_stat, pvalue = levene(df["Purchase_control"],
 print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
 
 
+#######################################################
+# Selecting and applying the appropriate test according to the Assumption of Normality and Homogeneity of Variance results
+######################################################
 
-# Adım 2: Selecting and applying the appropriate test according to the Assumption of Normality and Homogeneity of Variance results
 test_stat, pvalue = ttest_ind(df["Purchase_control"],df["Purchase_test"],
                               equal_var=True)
 print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
@@ -148,6 +101,4 @@ print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
 # t test
 
 
-# Adım 2: Elde ettiğiniz test sonuçlarına göre müşteriye tavsiyede bulununuz.
-# iki teklif verme türü arasında purchase ortalamarı arasında istatistiksel olarak bir fark bulunmadığı için,
-# şirket iki teklif türünden maliyet olraak uygun olanını seçebilir.
+
